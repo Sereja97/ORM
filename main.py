d775f7b0370341e04e7541e594aac2f575a38606
Sereja97 <154069162+Sereja97@users.noreply.github.com>
2024-02-15 21:17:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import sqlalchemy as sq
+import configparser
+from sqlalchemy.orm import sessionmaker
+from models import create_tables, add_json, Publisher, Shop, Book, Stock, Sale
+from task2 import publisher_info
+
+settings = configparser.ConfigParser()
+DSN = "postgresql://postgres:postgres@localhost:5432/home_db_ORM"
+engine = sq.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+add_json(session)
+
+
+print('Введите имя или идентификатор издателя:')
+publisher = input()
+
+q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale)
+if publisher.isdigit():
+    query = q.filter(Publisher.id == publisher).all()
+else:
+    query = q.filter(Publisher.name == publisher).all()
+
+publisher = []
+for title, name, price, date_sale in query:
+    publisher.append([title, name, price, date_sale])
+
+publisher_info(publisher)
---
+++ task2.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+
+
+def publisher_info(name):
+    info = PrettyTable()
+    info.field_names = ["Название книги", "Название магазина", "Стоимость покупки", "Дата покупки"]
+    for i in name:
+        info.add_row(i)
+    print(info)
